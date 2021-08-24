87d70d4a9167f584f466a44c7793078b7fc6329a
Artem Stukalenko <stukalenko.artem@gmail.com>
2021-08-24 14:17:12 +0300

    Made first admin security filter

---
+++ src/main/java/com/artemstukalenko/project/library/filters/AdminPagesFilter.java
@@ -0,0 +1,34 @@
+package com.artemstukalenko.project.library.filters;
+
+import com.artemstukalenko.project.library.entity.User;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AdminPagesFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        User potentialTrespasser = (User) request.getSession().getAttribute("currentUser");
+
+        if (potentialTrespasser.getUserDetails().getAuthorityString().equals("ADMIN")) {
+            filterChain.doFilter(request, servletResponse);
+        } else {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "STOYAMBA");
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
