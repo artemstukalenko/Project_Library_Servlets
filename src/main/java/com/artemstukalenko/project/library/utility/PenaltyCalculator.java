2fc1865d135866221fd0593a2bd5aded6f75a5df
Artem Stukalenko <stukalenko.artem@gmail.com>
2021-08-30 16:44:03 +0300

    Added logger to penalty calculator & removed stack trace

--- src/main/java/com/artemstukalenko/project/library/utility/PenaltyCalculator.java
+++ src/main/java/com/artemstukalenko/project/library/utility/PenaltyCalculator.java
@@ -7,11 +7,33 @@ import com.artemstukalenko.project.library.entity.User;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class PenaltyCalculator {
+
+    private final static Logger LOGGER;
+    private static FileHandler FILE_HANDLER;
+
+    static {
+        try {
+            FILE_HANDLER = new FileHandler("D:\\project_library_servlets\\src\\main\\resources\\logs\\penaltyLog.log",
+                    true);
+            FILE_HANDLER.setFormatter(new SimpleFormatter());
+            FILE_HANDLER.setLevel(Level.ALL);
+            FILE_HANDLER.setEncoding("UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        LOGGER = Logger.getLogger("PenaltyCalculator");
+        LOGGER.addHandler(FILE_HANDLER);
+    }
 
     @Resource(name = "jdbc/library_db")
     private DataSource dataSource;
@@ -22,26 +44,27 @@ public class PenaltyCalculator {
         this.dataSource = dataSource;
     }
 
-    public int calculateUsersPenalty(String username) {
+    public int calculateUsersPenalty(String username) throws SQLException {
         List<Subscription> userSubscriptions = new ArrayList<>();
 
-        try {
-            subscriptionDAO = new SubscriptionDAOImpl(dataSource);
-            userSubscriptions = subscriptionDAO.getUserSubscriptions(username);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        subscriptionDAO = new SubscriptionDAOImpl(dataSource);
+        userSubscriptions = subscriptionDAO.getUserSubscriptions(username);
 
-        return (int)(userSubscriptions.stream()
+
+        int penalty = (int)(userSubscriptions.stream()
                 .filter(subscription -> {return subscription.getExpired() && !subscription.getFined();})
                 .peek(subscription -> {
                     try {
                         subscriptionDAO.updateFinedInfo(subscription.getSubscriptionId(), true);
                     } catch (SQLException e) {
-                        e.printStackTrace();
+                        LOGGER.log(Level.SEVERE, "Failed to calculate user's penalty by subscription: " + subscription);
                     }
                 })
                 .count() * 10);
+
+        FILE_HANDLER.close();
+
+        return penalty;
     }
 
 }
